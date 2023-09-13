445a0530a2b0cb319f78082f5c5eea2f53fd34cd
Shafa.Auliya21 <shafa.auliya21@students.unila.ac.id>
2023-09-13 16:37:41 +0700

    Ubah Routes

---
+++ app/Views/profile.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile</title>
+</head>
+<body>
+<table>
+  <tr>
+    <th>Nama</th>
+    <th>:</th>
+    <th><?= $nama ?></th>
+  </tr>
+  <tr>
+    <td>Kelas</td>
+    <td>:</td>
+    <th><?= $kelas ?></th>
+  </tr>
+  <tr>
+    <td>NPM</td>
+    <td>:</td>
+    <th><?= $npm ?></th>
+  </tr>
+</table>
+</body>
+</html>
