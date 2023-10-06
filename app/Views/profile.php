35effb0e777029900c1fcd698e8a12544ef50017
Shafa.Auliya21 <shafa.auliya21@students.unila.ac.id>
2023-10-06 07:38:51 +0700

    Membuat List User

--- app/Views/profile.php
+++ app/Views/profile.php
@@ -1,16 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Profile</title>
-    <link rel="stylesheet" href="<?= base_url("assets/css/style.css")?>">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
-</head>
-<body>
+<?= $this->extend('layouts/app')?>
+
+<?= $this->section('content')?>
     <div class="container">
         <div class="profile-box">
             <h3><b>Profil</b></h3>
@@ -30,6 +20,4 @@
         </div>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
+<?= $this->endSection()?>
